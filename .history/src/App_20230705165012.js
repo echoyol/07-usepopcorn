0e0ef843d38a37685503b3f524008b1bba185616
Sunny <sunsunsunny20@gmail.com>
2023-07-05 17:00:29 +0800

    split components

--- .history/src/App_20230705165012.js
+++ .history/src/App_20230705165012.js
@@ -168,7 +168,7 @@ function WatchedBox() {
       {isOpen2 && (
         <>
           <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
+          <WatchedMovieList />
         </>
       )}
     </div>
@@ -204,41 +204,32 @@ function WatchedSummary({ watched }) {
   )
 }
 
-function WatchedMovieList({ watched }) {
+function WatchedMovieList() {
   return (
     <ul className='list'>
       {watched.map((movie) => (
-        <WatchedMovie
-          movie={movie}
-          key={movie.imdbID}
-        />
+        <li key={movie.imdbID}>
+          <img
+            src={movie.Poster}
+            alt={`${movie.Title} poster`}
+          />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+        </li>
       ))}
     </ul>
-  )
-}
-
-function WatchedMovie({ movie }) {
-  return (
-    <li>
-      <img
-        src={movie.Poster}
-        alt={`${movie.Title} poster`}
-      />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-      </div>
-    </li>
   )
 }
